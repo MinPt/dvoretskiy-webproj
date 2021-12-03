875f720a09f7b8a69c118f245b5c5dfe567484b4
MinPt <petr.minailyuk@gmail.com>
2021-12-03 20:24:20 +0200

    isAdmin checkbox added in registration form

--- src/backend/routes/users.js
+++ src/backend/routes/users.js
@@ -16,7 +16,7 @@ router.post("/", async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("User already registered");
 
-  user = new User(_.pick(req.body, ["name", "email", "password"]));
+  user = new User(_.pick(req.body, ["name", "email", "password", "isAdmin"]));
   const salt = await bcrypt.genSalt(10);
   user.password = await bcrypt.hash(user.password, salt);
 
@@ -24,7 +24,7 @@ router.post("/", async (req, res) => {
 
   const token = user.generateAuthToken();
 
-  const newUser = _.pick(user, ["_id", "name", "email"]);
+  const newUser = _.pick(user, ["_id", "name", "email", "isAdmin"]);
 
   res.header("x-auth-token", token).send({ ...newUser, jwtToken: token });
 });
